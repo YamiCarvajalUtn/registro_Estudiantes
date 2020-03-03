228f8c57a97730fd5ddb149b3d114848348df424
YamiCarvajalUtn <elixitabella.carvajal3msc@gmail.com>
2020-03-03 15:55:33 -0500

    feat: extender roles add source code

---
+++ Data/DummyData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using registroEstudiantes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace registroEstudiantes.Data
+{
+    public class DummyData
+    {
+        public static async Task Initialize(ApplicationDbContext context,
+                                            UserManager<ApplicationUser>userManager, 
+                                            RoleManager<ApplicationRole> roleManager)
+        {
+            context.Database.EnsureCreated();
+            string[] roleNames = { "admin", "estudiante", "docente" }; //array de los nombres de los roles 
+            foreach (var roleName in roleNames)
+            {
+                if (!roleManager.RoleExistsAsync(roleName).Result)//valida un true todo lo que no sea true mande a crear un rol
+                {
+                    ApplicationRole role = new ApplicationRole
+                    {
+                        Name = roleName,
+                        NormalizedName = roleName
+                    };
+                    roleManager.CreateAsync(role).Wait();
+                }
+            }
+
+            if (await userManager.FindByEmailAsync("admin@gmail.com") == null)//creamo un usuario
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    UserName = "admin@gmail.com",
+                    Email = "admin@gmail.com",
+                    EmailConfirmed = true,
+                    LockoutEnabled = false
+                };
+
+                IdentityResult result = userManager.CreateAsync(user, "Admin1234*").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "admin").Wait();
+                }
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,6 @@ namespace registroEstudiantes
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -36,19 +35,24 @@ namespace registroEstudiantes
             });
             services.AddDbContext<registroEstudiantesContext>(options => 
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
+                 
+                    
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
+            // services.AddDefaultIdentity<IdentityUser>()
+            //   .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options => options.Stores.MaxLengthForKeys = 128)
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddDefaultUI().AddDefaultTokenProviders();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env ,
+                              ApplicationDbContext context, 
+                              RoleManager<ApplicationRole> roleManager,
+                              UserManager<ApplicationUser> userManager)
         {
             if (env.IsDevelopment())
             {
@@ -73,6 +77,8 @@ namespace registroEstudiantes
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            DummyData.Initialize(context, userManager, roleManager).Wait();
         }
     }
 }
